4ad460056991e325b7f7976e86aca5d686532ae6
Shiqi Wang <shiwa@microsoft.com>
2017-10-19 18:29:52 -0700

    Verify loadTextureId works

---
+++ android/AndroidOpenGLESLessons/app/src/main/java/com/learnopengles/android/common/ImageLoader.java
@@ -0,0 +1,43 @@
+package com.learnopengles.android.common;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.opengl.GLES20;
+import android.opengl.GLUtils;
+import android.util.Log;
+
+import java.nio.ByteBuffer;
+
+public class ImageLoader {
+    private static final String TAG = ImageLoader.class.getSimpleName();
+
+    public static Bitmap loadBitmap(String fileName) {
+        Log.i(TAG, "loadBitmap path: " + fileName);
+
+        Bitmap bitmap = BitmapFactory.decodeFile(fileName);
+        return bitmap;
+    }
+
+    public static int loadTextureId(String fileName) {
+        Log.d(TAG, "loadTextureId path: " + fileName);
+        Bitmap bitmap = BitmapFactory.decodeFile(fileName);
+//        Log.d(TAG, "Bitmap is: " + bitmap);
+        ByteBuffer buffer = ByteBuffer.allocate(bitmap.getByteCount());
+        bitmap.copyPixelsToBuffer(buffer);
+        int textures[] = new int[1];
+        GLES20.glGenTextures(1, textures, 0);
+        int textureId = textures[0];
+        GLES20.glActiveTexture(GLES20.GL_TEXTURE0);
+        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, textureId);
+        GLUtils.texImage2D(GLES20.GL_TEXTURE_2D,0, bitmap,0);
+        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_LINEAR);
+        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_LINEAR);
+        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_WRAP_S,
+                GLES20.GL_CLAMP_TO_EDGE);
+        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_WRAP_T,
+                GLES20.GL_CLAMP_TO_EDGE);
+        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, 0);
+        Log.d(TAG, "texture id returned: " + textureId);
+        return textureId;
+    }
+}
